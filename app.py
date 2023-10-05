b72cbda6ed87bcc8b311aa26ab7459953f95f37b
Martin Panacek <79920088+martinpanacek789@users.noreply.github.com>
2023-10-05 18:00:14 +0200

    edit

--- app.py
+++ app.py
@@ -11,9 +11,7 @@ pdf_url = 'https://storage.googleapis.com/test-public-1234567/autentista_champag
 #    base64_pdf = base64.b64encode(f.read()).decode('utf-8')
 
 # Embedding PDF in HTML
-pdf_display = F'<iframe src="{pdf_url}" width="100%" height="600"></iframe>'
+pdf_display = F'<iframe class="content_viewport" src="{pdf_url}" width="100%" height="600"></iframe>'
 
 # Displaying File
 st.markdown(pdf_display, unsafe_allow_html=True)
-
-st.markdown("all good")
